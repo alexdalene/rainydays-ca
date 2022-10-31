82c50f02a34fc720071e617ee3fbfa80d0485ded
Alex <111867370+alexdalene@users.noreply.github.com>
2022-10-31 15:47:43 +0100

    redo jacket page?

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,17 +1,6 @@
 let prevScrollpos = window.pageYOffset;
 
-// window.onscroll = function() {
-//     let currentScrollpos = window.pageYOffset;
-//     if (prevScrollpos > currentScrollpos) {
-//         document.querySelector(".navbar").style.top = "0";
-//     } else {
-//         document.querySelector(".navbar").style.top = "-50px";
-//     }
-//     prevScrollpos = currentScrollpos;
-
-// }
-
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) { // js gets called only on these devices
     window.onscroll = function() {
         let currentScrollpos = window.pageYOffset;
         if (prevScrollpos > currentScrollpos) {
