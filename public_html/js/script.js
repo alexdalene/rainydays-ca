bac739c7e4bf388a1f4374838074bcd9384cd761
Alex <111867370+alexdalene@users.noreply.github.com>
2022-10-31 15:00:57 +0100

    experimental change

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,12 +1,25 @@
 let prevScrollpos = window.pageYOffset;
 
-window.onscroll = function() {
-    let currentScrollpos = window.pageYOffset;
-    if (prevScrollpos > currentScrollpos) {
-        document.querySelector(".navbar").style.top = "0";
-    } else {
-        document.querySelector(".navbar").style.top = "-50px";
-    }
-    prevScrollpos = currentScrollpos;
+// window.onscroll = function() {
+//     let currentScrollpos = window.pageYOffset;
+//     if (prevScrollpos > currentScrollpos) {
+//         document.querySelector(".navbar").style.top = "0";
+//     } else {
+//         document.querySelector(".navbar").style.top = "-50px";
+//     }
+//     prevScrollpos = currentScrollpos;
 
+// }
+
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+    window.onscroll = function() {
+        let currentScrollpos = window.pageYOffset;
+        if (prevScrollpos > currentScrollpos) {
+            document.querySelector(".navbar").style.top = "0";
+        } else {
+            document.querySelector(".navbar").style.top = "-50px";
+        }
+        prevScrollpos = currentScrollpos;
+    
+    }
 }
